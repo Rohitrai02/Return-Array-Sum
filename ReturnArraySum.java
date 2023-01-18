a17cd80d3cfea9109b43198c01b48013aff3ec10
Rohitrai02 <121882145+Rohitrai02@users.noreply.github.com>
2023-01-18 19:17:52 +0530

    Add files via upload

---
+++ ReturnArraySum.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ReturnArraySum {
+	public static int [] takeInput(){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=s.nextInt();
+		}
+		return arr;
+	}
+	public static int sum(int [] arr) {
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum+arr[i];   
+		}
+		return sum;	
+	}		
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		while(t>0) {
+			int[] arr=takeInput();
+			System.out.println(sum(arr));
+			t-=1;
+		}
+	}
+}
